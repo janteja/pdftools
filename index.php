b52f4e4ac6f44c17b7137fb3fb0fbd9b37433b13
jteja <jteja1@hotmail.com>
2018-02-22 22:53:01 +0000

    added loading animation

--- index.php
+++ index.php
@@ -11,7 +11,18 @@ $html = <<<XYZ
  <link rel="stylesheet" type="text/css" href="css/component.css"/>
  <script src="js/modernizr.custom.js"></script> 
 <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
- 
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("#compress").click(function(){
+        $("#div1").load("loading.txt");
+    });
+    $("#merge").click(function(){
+        $("#div1").load("loading.txt");
+    });
+});
+</script>
 <script src="js/dropzone.js"></script>
 <style>
 body {
@@ -22,22 +33,28 @@ text-decoration: none;
         text-shadow: 0 0 1px rgba(255,255,255,0.3);
         font-size: 1.1em;
 }
+.loading {
+position: absolute;
+backgroun-color:#3982c7;
+right: 47%;
+top: 40%;
+}
 </style>
 </head>
  
 <body>
+<div class="loading" id="div1"></div>
  <div>
 <form action="upload.php" class="dropzone"></form> 
 </div>
                    <nav class="cl-effect-7">
 
 <ul>
-    <li>  <a href="merge.php" datahover="Merge">Merge</a> </li>
+    <li>  <a href="merge.php" id="merge" datahover="Merge">Merge</a> </li>
         <li>  <a href="selection.php" id="remove"datahover="Remove Pages">Remove Pages</a> </li>
 	<li> <a href="compress.php" id="compress" datahover="Compress">Compress</a> </li>
 </ul>
                               </nav>
-
 </body>
 </html>
 XYZ;
